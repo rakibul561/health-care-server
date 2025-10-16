9cb8117b21f769e6963baee270a7caa854fe1391
Rakibul hasan <rakibulhasan3929@gmail.com>
2025-10-16 10:51:28 -0700

    updated code

--- src/app/module/schedule/schedule.service.ts
+++ src/app/module/schedule/schedule.service.ts
@@ -1,7 +1,29 @@
- 
+ import { addMinutes,addHours ,format } from "date-fns";
  const InsertIntoDb = (payload:any) =>{
+   
+   
+    const {startDate, endDate, startTime,endTime} = payload;
 
-    console.log(payload)
+    const intervelTime = 30;
+
+     const currentDate = new Date(startDate)
+     const lastDate = new Date(endDate)
+
+     while(currentDate <= lastDate){
+        const startDateTime = new Date (
+            addMinutes(
+                addHours(
+                    `${format(currentDate, "yyyy-mm-dd")}`,
+                    Number(startTime.split(":")[0])
+                ),
+                  Number(startTime.split(":")[1])
+            )
+
+
+
+        )
+     }
+
     return payload
 
  }
