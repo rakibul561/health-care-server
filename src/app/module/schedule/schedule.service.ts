0bdcdeaaf8fcb22cde65d2f711b1d646c9c31816
Rakibul hasan <rakibulhasan3929@gmail.com>
2025-10-17 10:27:51 -0700

    change code

--- src/app/module/schedule/schedule.service.ts
+++ src/app/module/schedule/schedule.service.ts
@@ -1,10 +1,12 @@
  import { addMinutes,addHours ,format } from "date-fns";
- const InsertIntoDb = (payload:any) =>{
+import { prisma } from "../../shared/prisma";
+ const InsertIntoDb = async (payload:any) =>{
    
    
     const {startDate, endDate, startTime,endTime} = payload;
 
     const intervelTime = 30;
+    const schedules = []
 
      const currentDate = new Date(startDate)
      const lastDate = new Date(endDate)
@@ -19,12 +21,49 @@
                   Number(startTime.split(":")[1])
             )
 
-
-
         )
+        const endDateTime = new Date (
+            addMinutes(
+                addHours(
+                    `${format(currentDate, "yyyy-mm-dd")}`,
+                    Number(endTime.split(":")[0])
+                ),
+                  Number(endTime.split(":")[1])
+            )
+        )
+
+
+        while (startDateTime <= endDateTime){
+           const slotStartDateTime = startDateTime;
+           const slotEndDateTime = addMinutes(startDateTime,intervelTime)
+
+           const scheduleData = {
+            startDateTime : slotStartDateTime,
+            endDateTime: slotEndDateTime
+           }
+
+           const existingSchedule = await prisma.schedule.findFirst({
+            where : scheduleData
+           })
+
+           if(!existingSchedule){
+            const result = await prisma.schedule.create({
+               data : scheduleData
+            })
+            schedules.push(result)
+           }
+           slotStartDateTime.setMinutes(slotStartDateTime.getMinutes() + intervelTime)
+
+
+
+        }
+
+        currentDate.setDate(currentDate.getDate() + 1 )
+
+
      }
 
-    return payload
+    return schedules
 
  }
 
